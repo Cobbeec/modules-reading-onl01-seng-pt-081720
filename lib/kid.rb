104b1e6716d9ba42955d81c09da66993b89543ea
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-19 23:45:32 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-FancyDance::ClassMethods
+extend FancyDance::ClassMethods
 include FancyDance::InstanceMethods
  attr_accessor :name
 
